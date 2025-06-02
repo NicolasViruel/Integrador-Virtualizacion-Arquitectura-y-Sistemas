c44c1f59aa7678cc3cc2d22ddf0ca7e60a4bc1b6
nicolas <nicolas.viruel@phinxlab.com>
2025-06-02 13:52:24 -0300

    Se agrega el codigo que ejecutamos en la consola del sistema operativo una vez que instalamos python

---
+++ promedio.py
@@ -0,0 +1,12 @@
+# Solicitar valores al usuario
+
+valor1 = float(input("Ingrese el primer valor: "))
+valor2 = float(input("Ingrese el segundo valor: "))
+valor3 = float(input("Ingrese el tercer valor: "))
+
+# Calcular el promedio
+
+promedio = (valor1 + valor2 + valor3) / 3
+
+# Mostramos el resultado
+print(f"El promedio de los valores es: {promedio:.2f}")
